9662d06f3ab4a2a2305abed7c768385947e29fdc
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-05-08 12:34:21 -0600

    starting header

--- lib/src/Pages/Components/Header.jsx
+++ lib/src/Pages/Components/Header.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div className="header--container">
-        <p>Header Component Test</p>
+        <div className="header--content">
+            <Link to="/" className="header--logo__link">
+                <div className="header--logo__container">
+                    <img src={require('./img/logo.png')} alt="" className="header--logo" />
+                </div>
+                <p className="header--logo--title">StockHive</p>
+            </Link>
+        </div>
     </div>
   );
 }
