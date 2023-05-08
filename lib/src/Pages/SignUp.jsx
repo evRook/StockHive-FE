0180e973e928e8debd8d5cc22fd9936b186d1161
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-05-08 15:09:54 -0600

    signup css wire

--- lib/src/Pages/SignUp.jsx
+++ lib/src/Pages/SignUp.jsx
@@ -4,44 +4,49 @@ import './SignUp.css'
 export default function SignUp() {
     return ( 
         <div className="signup--container">
-            <div className="sigup--title__container">
-                <p className="sigup--title">
-                    Sign Up
-                </p>
+            <div className="signup--img__container">
+
             </div>
-            <div className="signup--form__container">
-                <form className='signup--form'>
-                    <label>First</label>
-                    <input 
-                        name='First Name'
-                        type="text" 
-                    />
-                    <label>Last</label>
-                    <input 
-                        name='Last Name'
-                        type="text" 
-                    />
-                    <label>Email</label>
-                    <input 
-                        name='Email'
-                        type="text" 
-                    />
-                    <label>User Name</label>
-                    <input 
-                        name='User Name'
-                        type="text" 
-                    />
-                    <label>Password:</label>
-                    <input 
-                        name='Password'
-                        type="password" 
-                    />
-                    <label>Confirm Password:</label>
-                    <input 
-                        name='Confirm Password'
-                        type="password confirm" 
-                    />
-                </form>
+            <div className="signup--content__container">
+                <div className="sigup--title__container">
+                    <p className="sigup--title">
+                        Sign Up
+                    </p>
+                </div>
+                <div className="signup--form__container">
+                    <form className='signup--form'>
+                        <label>First</label>
+                        <input 
+                            name='First Name'
+                            type="text" 
+                        />
+                        <label>Last</label>
+                        <input 
+                            name='Last Name'
+                            type="text" 
+                        />
+                        <label>Email</label>
+                        <input 
+                            name='Email'
+                            type="text" 
+                        />
+                        <label>User Name</label>
+                        <input 
+                            name='User Name'
+                            type="text" 
+                        />
+                        <label>Password:</label>
+                        <input 
+                            name='Password'
+                            type="password" 
+                        />
+                        <label>Confirm Password:</label>
+                        <input 
+                            name='Confirm Password'
+                            type="password confirm" 
+                        />
+                    </form>
+                </div>
             </div>
         </div>
      );
