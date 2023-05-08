10ceaf2e90f57ff78924c44ab0ccf55a4bef6a62
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-05-08 15:00:41 -0600

    sigup wire

--- lib/src/Pages/SignUp.jsx
+++ lib/src/Pages/SignUp.jsx
@@ -3,6 +3,46 @@ import './SignUp.css'
 
 export default function SignUp() {
     return ( 
-        <p>Signup world</p>
+        <div className="signup--container">
+            <div className="sigup--title__container">
+                <p className="sigup--title">
+                    Sign Up
+                </p>
+            </div>
+            <div className="signup--form__container">
+                <form className='signup--form'>
+                    <label>First</label>
+                    <input 
+                        name='First Name'
+                        type="text" 
+                    />
+                    <label>Last</label>
+                    <input 
+                        name='Last Name'
+                        type="text" 
+                    />
+                    <label>Email</label>
+                    <input 
+                        name='Email'
+                        type="text" 
+                    />
+                    <label>User Name</label>
+                    <input 
+                        name='User Name'
+                        type="text" 
+                    />
+                    <label>Password:</label>
+                    <input 
+                        name='Password'
+                        type="password" 
+                    />
+                    <label>Confirm Password:</label>
+                    <input 
+                        name='Confirm Password'
+                        type="password confirm" 
+                    />
+                </form>
+            </div>
+        </div>
      );
 }
