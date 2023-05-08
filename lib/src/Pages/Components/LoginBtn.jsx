22f3584986165c6824aef3c493299423ce68c347
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-05-08 12:38:34 -0600

    login btn component

---
+++ lib/src/Pages/Components/LoginBtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoginBtn.css'
+
+export default function LoginBtn() {
+    return ( 
+        <div className="login--container">
+            <button className="login--button">
+                Log in
+            </button>
+        </div>
+     );
+}
