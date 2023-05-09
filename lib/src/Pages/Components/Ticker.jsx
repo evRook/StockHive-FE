b7d0b287d04da965963fb4aa0c19c7f9b64795bf
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-05-09 12:37:13 -0600

    woring on ticker scrolling

---
+++ lib/src/Pages/Components/Ticker.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import './Ticker.css'
+
+export default function Ticker() {
+    return ( 
+        <div className="ticker--container">
+            <div className="ticker--content">
+                <div className="ticker--card__container">
+                    <p className="ticker--card--title">AAPL</p>
+                    <div className="ticker--card__content">
+                        <p className="ticker--open">125.46</p>
+                        <p className="ticker--close">126.77</p>
+                    </div>
+                </div>
+                <div className="ticker--card__container">
+                    <p className="ticker--card--title">AAPL</p>
+                    <div className="ticker--card__content">
+                        <p className="ticker--open">125.46</p>
+                        <p className="ticker--close">126.77</p>
+                    </div>
+                </div>
+                <div className="ticker--card__container">
+                    <p className="ticker--card--title">AAPL</p>
+                    <div className="ticker--card__content">
+                        <p className="ticker--open">125.46</p>
+                        <p className="ticker--close">126.77</p>
+                    </div>
+                </div>
+                <div className="ticker--card__container">
+                    <p className="ticker--card--title">AAPL</p>
+                    <div className="ticker--card__content">
+                        <p className="ticker--open">125.46</p>
+                        <p className="ticker--close">126.77</p>
+                    </div>
+                </div>
+                <div className="ticker--card__container">
+                    <p className="ticker--card--title">AAPL</p>
+                    <div className="ticker--card__content">
+                        <p className="ticker--open">125.46</p>
+                        <p className="ticker--close">126.77</p>
+                    </div>
+                </div>
+                <div className="ticker--card__container">
+                    <p className="ticker--card--title">AAPL</p>
+                    <div className="ticker--card__content">
+                        <p className="ticker--open">125.46</p>
+                        <p className="ticker--close">126.77</p>
+                    </div>
+                </div>
+                <div className="ticker--card__container">
+                    <p className="ticker--card--title">AAPL</p>
+                    <div className="ticker--card__content">
+                        <p className="ticker--open">125.46</p>
+                        <p className="ticker--close">126.77</p>
+                    </div>
+                </div>
+                <div className="ticker--card__container">
+                    <p className="ticker--card--title">AAPL</p>
+                    <div className="ticker--card__content">
+                        <p className="ticker--open">125.46</p>
+                        <p className="ticker--close">126.77</p>
+                    </div>
+                </div>
+                <div className="ticker--card__container">
+                    <p className="ticker--card--title">AAPL</p>
+                    <div className="ticker--card__content">
+                        <p className="ticker--open">125.46</p>
+                        <p className="ticker--close">126.77</p>
+                    </div>
+                </div>
+                <div className="ticker--card__container">
+                    <p className="ticker--card--title">AAPL</p>
+                    <div className="ticker--card__content">
+                        <p className="ticker--open">125.46</p>
+                        <p className="ticker--close">126.77</p>
+                    </div>
+                </div>
+                <div className="ticker--card__container">
+                    <p className="ticker--card--title">AAPL</p>
+                    <div className="ticker--card__content">
+                        <p className="ticker--open">125.46</p>
+                        <p className="ticker--close">126.77</p>
+                    </div>
+                </div>
+                <div className="ticker--card__container">
+                    <p className="ticker--card--title">AAPL</p>
+                    <div className="ticker--card__content">
+                        <p className="ticker--open">125.46</p>
+                        <p className="ticker--close">126.77</p>
+                    </div>
+                </div>
+                <div className="ticker--card__container">
+                    <p className="ticker--card--title">AAPL</p>
+                    <div className="ticker--card__content">
+                        <p className="ticker--open">125.46</p>
+                        <p className="ticker--close">126.77</p>
+                    </div>
+                </div>
+                <div className="ticker--card__container">
+                    <p className="ticker--card--title">AAPL</p>
+                    <div className="ticker--card__content">
+                        <p className="ticker--open">125.46</p>
+                        <p className="ticker--close">126.77</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- lib/src/Pages/Home.jsx
+++ lib/src/Pages/Home.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import "./Home.css";
-import { WireBtn, SolidBtn, NavBtn } from './Components'
+import { WireBtn, SolidBtn, NavBtn, Ticker } from './Components'
+import { Link } from "react-router-dom";
 
 export default function Home() {
     return ( 
         <div className="home--container">
             <div className="home--header__container">
-                <div className="home--header--img">
-
-                </div>
+                <img src={require('./Components/img/hexagon.webp')} alt="" className="home--header--img"/>
                 <div className="home--header--content">
                     <p className="home--header--text">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?" 
                     </p>
-                    <WireBtn title='about' />
+                    <Link to="/about" className='header--link'>
+                        <WireBtn title='About' />
+                    </Link>
                 </div>
             </div>
             <div className="home--ticker__container">
-                <p className="ticker--placeholder">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                </p>
+                <Ticker />
             </div>
             <div className="home--content__container">
                     
