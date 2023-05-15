5e56fa0f9df225c4acaff3c0257c0f6637a05c73
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-05-15 17:38:45 -0600

    verify now talks to backend

--- lib/src/Pages/Components/WireBtn.jsx
+++ lib/src/Pages/Components/WireBtn.jsx
@@ -4,7 +4,7 @@ import './WireBtn.css'
 export default function WireBtn(props) {
     return ( 
         <div className="wire--btn__container">
-            <button className="wire--btn">
+            <button className="wire--btn" type={props.type} onClick={props.onClick}>
                 {props.title}
             </button>
         </div>
--- lib/src/Pages/Verify.jsx
+++ lib/src/Pages/Verify.jsx
@@ -1,8 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Verify.css'
+import { useNavigate, useParams } from "react-router-dom";
+import { WireBtn } from './Components'
+import axios from 'axios'
+import Cookies from 'js-cookie';
+
 
 export default function Verify() {
+    const navigate = useNavigate()
+
+    const { uid, token } = useParams()
+
+    function handleSubmit(){
+        
+        const data = {
+            uid: uid,
+            token: token
+        }
+
+        const csrfToken = Cookies.get('csrftoken')
+
+        const headers = {
+            headers: {
+                'Content-type': 'application/json',
+                'X-CSRFToken': csrfToken,
+            }
+        }
+
+        const dataStr = JSON.stringify(data)
+
+        axios.post('http://localhost:8000/auth/users/activation/', dataStr, headers)
+            .then((response) => {
+                response.json()
+            })
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
+            navigate('/login')
+    }
+
     return ( 
-        <p>Hello Verify</p>
+        <div className="verify--container">
+            <p className="verify--text">
+                Verify your account to start tracking trades!
+            </p>
+            <WireBtn 
+                title='Verify'
+                type='submit' 
+                onClick={handleSubmit}
+            />
+        </div>
      );
 }
