6318fbd10c4ee403a1c2e7a7d67d30a820923608
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-05-15 18:08:19 -0600

    connecting reset password to backend

--- lib/src/Pages/PWReset.jsx
+++ lib/src/Pages/PWReset.jsx
@@ -1,8 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PWReset.css'
+import { useParams } from "react-router-dom";
+import axios from 'axios'
+import Cookies from 'js-cookie';
 
 export default function PWReset() {
+
+    const { uid, token } = useParams()
+
+    const [ email, setEmail ] = useState('')
+
+
+    function handleSubmit(){
+        
+        const data = {
+            uid: uid,
+            token: token,
+            email: email
+        }
+
+        const csrfToken = Cookies.get('csrftoken')
+
+        const headers = {
+            headers: {
+                'Content-type': 'application/json',
+                'X-CSRFToken': csrfToken,
+            }
+        }
+
+        const dataStr = JSON.stringify(data)
+
+        axios.post('http://localhost:8000/auth/users/reset_password/', dataStr, headers)
+            .then((response) => {
+                response.json()
+            })
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+
     return ( 
-        <p>Hello PWReset</p>
+        <div className="PWReset--container">
+            <div className="PWReset--content__container">
+                <div className="PWReset--title__container">
+                    <p className="PWReset  --title">Reset Password</p>
+                </div>
+                <div className="PWReset--form__container">
+                    <form 
+                        className='PWReset--form' 
+                        onSubmit = {handleSubmit}
+                    >
+                        <label>Email:</label>
+                        <input 
+                            name='email'
+                            type='email'
+                            placeholder='Email'
+                            value={email}
+                            onChange={(e) => {setEmail(e.target.value)}}
+                            required 
+                        />
+                        <button type='submit'>Log In</button>
+                    </form>
+                </div>
+            </div>
+        </div>
      );
 }
