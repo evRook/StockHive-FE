0d4c92e4b9fe2057f5a4e70da61be00b39bd4ca3
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-05-19 19:54:51 -0600

    more home css

--- lib/src/Pages/Home.jsx
+++ lib/src/Pages/Home.jsx
@@ -12,25 +12,33 @@ export default function Home(props) {
 
     return ( 
         <div className="home--container">
-            <div className="home--header__container">
+            <div className="home--ticker__container">
+                <Ticker data={companyData}/>
+            </div>
+            <div className="home--header__container">  
                 <div className="home--header--content">
-                    <p className="home--header--text">
+                    <p className="home--header--title">
                         StockHive 
-                        <br/>
-                        <br/>
+                    </p>
+                    <p className="home--header--text">
+                        Find Clarity in Investing
+                    </p>
+                </div>
+                <img src={require('./Components/img/logo.png')} alt="" className='home--header--img'/>
+            </div>
+            
+            <div className="home--content__container">
+                <div className="home--content--1">
+                    <p className="home--header--text">
                         Your go-to source for monitoring your investments in the stock market. Our app provides real-time updates on stock prices and allows you to create a personalized watchlist of your favorite stocks, so you can stay informed and make informed decisions.
                     </p>
                     <Link to="/about" className='header--link'>
                         <WireBtn title='About' />
                     </Link>
                 </div>
-                <div className="home--ticker__container">
-                        <Ticker data={companyData}/>
-                </div>
-            </div>
-            
-            <div className="home--content__container">
+                <div className="home--content--2">
                     
+                </div>
             </div>
         </div>
      );
