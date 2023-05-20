9e18e14ac3085e676337b6b891db10195e65b282
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-05-20 00:33:39 -0600

    quick mirror css for contact form

--- lib/src/Pages/Contact.jsx
+++ lib/src/Pages/Contact.jsx
@@ -4,37 +4,45 @@ import './Contact.css'
 export default function Contact() {
     return ( 
         <div className="contact--container">
-            <div className="contact--content__container">
-                <div className="contact--title__container">
-                    <p className="contact--title">
-                        Contact Us
-                    </p>
+            <div className="contact--sub__container">
+                <div className="contact--img__container">
                 </div>
-                <div className="contact--form__container">
-                    <form 
-                        className='contact--form'
-                    >
-                        <label>Name:</label>
-                        <input 
-                            name='name'
-                            type='text'
-                            placeholder='Name'
-                        />
-                        <label>Email:</label>
-                        <input 
-                            name='email'
-                            type='email'
-                            placeholder='Email'
-                        />
-                        <label>Message:</label>
-                        <textarea 
-                            name='comments'
-                            type='text'
-                            placeholder='1000 Character Limit*'
-                            maxLength='1000'
-                        />
-                        <button type='submit'>Sign Up</button>
-                    </form>
+                <div className="contact--content__container">
+                    <div className="contact--title__container">
+                        <p className="contact--title">
+                            Contact Us
+                        </p>
+                        <img src={require('./Components/img/logo.png')} alt="" className="login--logo" />
+                    </div>
+                    <div className="contact--form__container">
+                        <form 
+                            className='contact--form'
+                        >
+                            <label>Name:</label>
+                            <input 
+                                name='name'
+                                type='text'
+                                placeholder='Name'
+                                className='contact--form--input'
+                            />
+                            <label>Email:</label>
+                            <input 
+                                name='email'
+                                type='email'
+                                placeholder='Email'
+                                className='contact--form--input'
+                            />
+                            <label>Message:</label>
+                            <textarea 
+                                name='comments'
+                                type='text'
+                                placeholder='1000 Character Limit*'
+                                maxLength='1000'
+                                className='contact--form--textField'
+                            />
+                            <button type='submit' className='contact--form--btn'>Send</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
